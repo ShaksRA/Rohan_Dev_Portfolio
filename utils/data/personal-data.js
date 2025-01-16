c0bfd7f97778a23ed1bfcb7ad37ed4a706335b3f
Rohan Agarwal <21052610@kiit.ac.in>
2025-01-16 15:37:48 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -7,7 +7,7 @@ export const personalData = {
   phone: '+918090940234',
   address: 'Rajahmundry, Andhra Pradesh, India - 533106 ',
   github: 'https://github.com/ShaksRA',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  facebook: 'https://www.facebook.com/rohan.agarwal.5895?mibextid=ZbWKwL',
   linkedIn: 'https://www.linkedin.com/in/rohan-agarwal-6056ab294/',
   twitter: 'https://twitter.com/rohan221102',
   stackOverflow: 'https://stackoverflow.com/users/16840768/ShaksRA',
